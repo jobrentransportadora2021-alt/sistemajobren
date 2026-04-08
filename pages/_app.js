ad1d2f383a0245e912ba5d4ddeb4b5d5547fa11c
jobrentransportadora2021-alt <jobrentransportadora2021@gmail.com>
2026-04-08 13:47:18 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css' // Se você tiver CSS, se não, pode deixar vazio
+"use client";
+import '../styles/globals.css'; // Se você não tiver esse arquivo, crie um vazio em styles/globals.css
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className="font-sans">
+      <Component {...pageProps} />
+    </main>
+  );
 }
 
-export default MyApp
+export default MyApp;
