6f9a957a6015c02dcd64488c967e79ec5f4e07a9
jobrentransportadora2021-alt <jobrentransportadora2021@gmail.com>
2026-04-08 15:06:18 -0300

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Força o roteamento das páginas
+  pagesDir: 'pages', 
+}
+
+module.exports = nextConfig
