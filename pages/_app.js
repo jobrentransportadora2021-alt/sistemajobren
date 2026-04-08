0632131b710e82948eeee3238412419b6ca33be2
jobrentransportadora2021-alt <jobrentransportadora2021@gmail.com>
2026-04-08 13:35:50 -0300

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import '../styles/globals.css' // Se você tiver CSS, se não, pode deixar vazio
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
