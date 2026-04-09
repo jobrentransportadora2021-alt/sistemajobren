889cb55e197154df07f94b31d5fee2bdbf2437f0
jobrentransportadora2021-alt <jobrentransportadora2021@gmail.com>
2026-04-09 13:19:48 -0300

    Delete next.config.js

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // Removemos a linha 'pagesDir' que estava causando o erro
-}
-
-module.exports = nextConfig
