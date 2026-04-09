6e2899db09dc504c5e773d6d1c6e1b2744358f27
jobrentransportadora2021-alt <jobrentransportadora2021@gmail.com>
2026-04-09 15:08:42 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
-// config.js - Conexão Real Jobren Logística
-const SUPABASE_URL = 'https://mcspwmajxfjzqfoxzvqu.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_QJG8VV2XRNfghNiylDQWcg_zUS8kQvE';
+// config.js - Evitando erro de redeclaração
+var supabaseUrl = 'https://mcspwmajxfjzqfoxzvqu.supabase.co';
+var supabaseKey = 'sb_publishable_QJG8VV2XRNfghNiylDQWcg_zUS8kQvE';
 
-// Inicializa o cliente do Supabase
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+var supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
